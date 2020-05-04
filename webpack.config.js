471250776e545ef99a870c4c54a6f04621b25eb7
CFPBot <CFPBot@users.noreply.github.com>
2020-05-04 21:50:31 +0000

    Update GitHub Pages

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,11 @@
 const path = require('path');
-const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   mode: process.env.NODE_ENV || 'development',
   entry: {
     'netlify-cms': './admin/src/netlify-cms.js',
-    'design-system': './assets/js/main.js'
+    'main': './assets/js/main.js',
+    'search': './assets/js/search.js'
   },
   output: {
     path: path.resolve(__dirname, 'dist', 'js'),
